39593e684f6569577288bd500a0a539a6d0e90fb
BitWizCoder <mhmdnoman01@gmail.com>
2025-11-21 15:17:07 +0600

    added shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 function App() {
   return (
-    <h1 className="text-center mt-14 text-4xl font-semibold">Hello, World!</h1>
+    <div>
+      <h1 className="text-center mt-14 text-4xl font-semibold">
+        Hello World!
+      </h1>
+    </div>
   );
 }
 
--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,3 +1,9 @@
+
+
 export default function Introduction() {
-  return <div></div>;
+  return (
+    <div>
+
+    </div>
+  );
 }
