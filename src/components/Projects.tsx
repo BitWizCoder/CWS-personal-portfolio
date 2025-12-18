b100fc9101380d6d815d7d2c517bb4e232aa91c0
BitWizCoder <mhmdnoman01@gmail.com>
2025-12-18 19:21:42 +0600

    added projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,32 +6,32 @@ function Projects() {
       <SectionIntro heading="My featured projects" subHeading="Projects" />
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        <div>
+        <div className="overflow-hidden rounded-md">
           <img
             src="./images/project-ph-1.jpeg"
             alt="project 1"
-            className="rounded-sm"
+            className="rounded-md transition duration-500 hover:scale-115 w-full"
           />
         </div>
-        <div>
+        <div className="overflow-hidden rounded-md">
           <img
             src="./images/project-ph-2.jpeg"
             alt="project 1"
-            className="rounded-sm"
+            className="rounded-md transition duration-500 hover:scale-115 w-full"
           />
         </div>
-        <div>
+        <div className="overflow-hidden rounded-md">
           <img
             src="./images/project-ph-3.jpeg"
             alt="project 1"
-            className="rounded-sm"
+            className="rounded-md transition duration-500 hover:scale-115 w-full"
           />
         </div>
-        <div>
+        <div className="overflow-hidden rounded-md">
           <img
             src="./images/project-ph-4.jpeg"
             alt="project 1"
-            className="rounded-sm"
+            className="rounded-md transition duration-500 hover:scale-115 w-full"
           />
         </div>
       </div>
