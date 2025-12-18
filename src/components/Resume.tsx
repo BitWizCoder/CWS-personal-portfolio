59b48e3002e83e4052cbd8e325f065da262dc3e7
BitWizCoder <mhmdnoman01@gmail.com>
2025-12-19 02:10:55 +0600

    added resume

---
+++ src/components/Resume.tsx
@@ -0,0 +1,17 @@
+import SectionIntro from "./shared/SectionIntro"
+
+function Resume() {
+  return (
+    <div>Resume
+
+        <SectionIntro heading="Education and practical experience
+" subHeading="Resume" />
+
+<p>With a background in Computer Science and hands-on experience in design and front-end development, I’ve worked on diverse projects ranging from landing pages to SaaS dashboards. Each project has strengthened my focus on building clean, functional, and user-friendly digital experiences.
+
+</p>
+    </div>
+  )
+}
+
+export default Resume
